ac142c578a465f4f61d091de9fea87a0e7a5b215
Your Name <your-email@example.com>
2026-05-28 14:42:25 +0530

    User login update

--- Servar/routes/settingsRoutes.js
+++ Servar/routes/settingsRoutes.js
@@ -1,27 +1,9 @@
 import express from "express";
 import Settings from "../models/Settings.js";
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
-
-/* =========================
-   GET SETTINGS
-========================= */
-router.get("/", async (req, res) => {
-  try {
-    let settings = await Settings.findOne();
-
-    // Agar database me settings nahi hai
-    if (!settings) {
-      settings = await Settings.create({});
-    }
-
-    res.status(200).json(settings);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
 
 /* =========================
    UPDATE SETTINGS
@@ -39,24 +21,34 @@ router.put("/", async (req, res) => {
 
     let settings = await Settings.findOne();
 
-    // Agar settings nahi mili
     if (!settings) {
       settings = new Settings();
     }
 
+    // OLD ADMIN USER
+    const adminUser = await User.findOne({ role: "admin" });
+
+    /* SETTINGS UPDATE */
     settings.studioName = studioName;
     settings.adminEmail = adminEmail;
-
-    // Password blank ho to old password rahega
-    if (password && password.trim() !== "") {
-      settings.password = password;
-    }
-
     settings.logo = logo;
     settings.themeColor = themeColor;
     settings.websiteDescription = websiteDescription;
 
     await settings.save();
+
+    /* ADMIN USER UPDATE */
+    if (adminUser) {
+      adminUser.email = adminEmail;
+
+      // password blank ho to old password rahega
+      if (password && password.trim() !== "") {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        adminUser.password = hashedPassword;
+      }
+
+      await adminUser.save();
+    }
 
     res.status(200).json({
       message: "Settings updated successfully",
